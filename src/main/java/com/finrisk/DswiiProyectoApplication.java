e3bba8759ddad85e0dfc605c2ccdbcaa2128a0f1
Gabriel <gabriel.devback@gmail.com>
2026-06-10 17:27:37 -0500

    Proyecto inicado

---
+++ src/main/java/com/finrisk/DswiiProyectoApplication.java
@@ -0,0 +1,13 @@
+package com.finrisk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DswiiProyectoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DswiiProyectoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/finrisk/DswiiProyectoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.finrisk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DswiiProyectoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
