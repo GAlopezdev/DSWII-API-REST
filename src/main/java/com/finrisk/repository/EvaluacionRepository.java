170449582fa5c0b602947b59aff120d5ee6f981d
Gabriel <gabriel.devback@gmail.com>
2026-06-19 02:22:13 -0500

    Revert "Cambio Johan 14/06/26"
    This reverts commit 9844f6005d25cbd4ca641d934be1db0dad90a6af.

--- src/main/java/com/finrisk/repository/EvaluacionRepository.java
+++
@@ -1,9 +0,0 @@
-package com.finrisk.repository;
-
-import com.finrisk.entity.EvaluacionFinanciera;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface EvaluacionRepository extends JpaRepository<EvaluacionFinanciera, Integer> {
-}
