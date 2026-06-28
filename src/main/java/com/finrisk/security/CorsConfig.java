c62ca8baf2ca35efddc7067e73072d6abb009e89
Gabriel <gabriel.devback@gmail.com>
2026-06-28 08:32:49 -0500

    feat. se agrego un nuevo dominio

--- src/main/java/com/finrisk/security/CorsConfig.java
+++ src/main/java/com/finrisk/security/CorsConfig.java
@@ -14,7 +14,7 @@ public class CorsConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:4200"));
+        configuration.setAllowedOrigins(List.of("http://localhost:4200","https://dswii-appweb.vercel.app"));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
